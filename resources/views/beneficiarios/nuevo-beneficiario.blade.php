b0924d391f11ab38eb2e07da66eb80c771343306
Luis Lucero <llucerob@gmail.com>
2023-03-15 17:30:48 -0300

    creacion medidas

--- resources/views/beneficiarios/nuevo-beneficiario.blade.php
+++ resources/views/beneficiarios/nuevo-beneficiario.blade.php
@@ -28,7 +28,7 @@
         <div class="col-sm-12">
             <div class="card">
                 <div class="card-header">
-                    <h5>A continuacion usted creara un nuevo beneficiario.</h5>
+                    <h5>A continuación usted creara un nuevo beneficiario.</h5>
                     
                 </div>
                 
@@ -38,16 +38,16 @@
                           <div class="row">
                             <div class="col">
                               <div class="mb-3">
-                                <label class="form-label" for="exampleFormControlInput1">Email address</label>
-                                <input class="form-control" id="exampleFormControlInput1" type="email" placeholder="name@example.com">
+                                <label class="form-label" for="inputNombre">Nombre</label>
+                                <input class="form-control" id="inputNombre" type="text" name="nombre" placeholder="Juan Alberto">
                               </div>
                             </div>
                           </div>
                           <div class="row">
                             <div class="col">
                               <div class="mb-3">
-                                <label class="form-label" for="exampleInputPassword2">Password</label>
-                                <input class="form-control" id="exampleInputPassword2" type="password" placeholder="Password">
+                                <label class="form-label" for="inputApellidos">Apellidos</label>
+                                <input class="form-control" id="inputApellidos" type="text" name="apellidos" placeholder="Perez Perez">
                               </div>
                             </div>
                           </div>
@@ -65,28 +65,8 @@
                               </div>
                             </div>
                           </div>
-                          <div class="row">
-                            <div class="col">
-                              <div class="mb-3">
-                                <label class="form-label" for="exampleFormControlSelect3">Example multiple select</label>
-                                <select class="form-select digits" id="exampleFormControlSelect3" multiple="">
-                                  <option>1</option>
-                                  <option>2</option>
-                                  <option>3</option>
-                                  <option>4</option>
-                                  <option>5</option>
-                                </select>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="row">
-                            <div class="col">
-                              <div>
-                                <label class="form-label" for="exampleFormControlTextarea4">Example textarea</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea4" rows="3"></textarea>
-                              </div>
-                            </div>
-                          </div>
+                          
+                          
                         </div>
                         <div class="card-footer text-end">
                           <button class="btn btn-primary" type="submit">Submit</button>
